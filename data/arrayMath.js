42fffd49078276483790de3a70001cec94f630f2
Clay Hibbard <38364771+Hibbarcd@users.noreply.github.com>
2018-10-06 16:48:53 -0500

    Add files via upload
    last chance to add to repo

---
+++ data/arrayMath.js
@@ -0,0 +1,54 @@
+const friendArray = require("./friends.js");
+
+let array1 = friendArray[0].scores;
+let array2 = friendArray[1].scores;
+
+console.log(array1);
+console.log(array2);
+// above here works====================================
+
+
+
+// let newFriend = require('');
+
+//  let finalArray = [];
+//   for(let i = 0; (i < 6); i++){
+
+//     finalArray.push(Math.abs(array1[i] - array2[i]))
+    
+//  }
+// console.log(finalArray[0],finalArray[1],finalArray[2],finalArray[3],finalArray[4],finalArray[5]);
+
+// for(let i = 0; (i < 6); i++){
+// var sum = [finalArray[i]].reduce(add, (i));
+
+// function add(a, b) {
+//     return (finalArray[0] + finalArray[1] + finalArray[2] + finalArray[3] + finalArray[4] + finalArray[5]);
+
+// }
+// }
+// console.log(sum);
+
+//=============================================================
+
+// //const array1 = ['4','3','1','5','1','4'];
+// //const array2 = ['5','5','2','1','3','5'];
+
+//  let finalArray = [];
+//   for(let i = 0; (i < 6); i++){
+
+//     finalArray.push(Math.abs(array1[i] - array2[i]))
+    
+//  }
+// console.log(finalArray[0],finalArray[1],finalArray[2],finalArray[3],finalArray[4],finalArray[5]);
+
+// for(let i = 0; (i < 6); i++){
+// var sum = [finalArray[i]].reduce(add, (i));
+
+// function add(a, b) {
+//     return (finalArray[0] + finalArray[1] + finalArray[2] + finalArray[3] + finalArray[4] + finalArray[5]);
+
+// }
+// }
+// console.log(sum);
+
--- data/friends.js
+++ data/friends.js
@@ -1,3 +1,5 @@
+
+
 const friendArray = [
     {
         name:"Sarah",
