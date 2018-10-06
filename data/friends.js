d3df1d06f1503b040d75c64716338ba4cba6abcb
Clay Hibbard <38364771+Hibbarcd@users.noreply.github.com>
2018-10-06 13:59:06 -0500

    Add files via upload
    Initial upload

---
+++ data/friends.js
@@ -0,0 +1,40 @@
+const friendArray = [
+    {
+        name:"Sarah",
+        photo:"#",
+        scores: [
+            '3',
+            '2',
+            '3',
+            '3',
+            '1',
+            '5',
+            '3',
+            '3',
+            '3',
+            '4',
+        ]
+        },
+    {
+        name:"Abbie",
+        photo:"#",
+        scores: [
+            '5',
+            '3',
+            '4',
+            '3',
+            '5',
+            '1',
+            '3',
+            '3',
+            '1',
+            '5',
+        ]
+        }
+
+    ];
+    module.exports = friendArray;
+
+
+
+    
