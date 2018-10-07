f4568249f274b2b601f0443aa59da6d905cc338d
Clay Hibbard <38364771+Hibbarcd@users.noreply.github.com>
2018-10-07 08:56:45 -0500

    Add files via upload
    Final upload with comments on unimplemented portion

--- data/arrayMath.js
+++ data/arrayMath.js
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 const friendArray = require("./friends.js");
 
 let array1 = friendArray[0].scores;
@@ -52,3 +53,59 @@ console.log(array2);
 // }
 // console.log(sum);
 
+=======
+const friendArray = require("./friends.js");
+
+let array1 = friendArray[0].scores;
+let array2 = friendArray[1].scores;
+
+console.log(array1);
+console.log(array2);
+// above here works====================================
+
+
+
+// let newFriend = require('');
+
+//  let finalArray = [];
+//   for(let i = 0; (i < 6); i++){
+
+//     finalArray.push(Math.abs(array1[i] - array2[i]))
+    
+//  }
+// console.log(finalArray[0],finalArray[1],finalArray[2],finalArray[3],finalArray[4],finalArray[5]);
+
+// for(let i = 0; (i < 6); i++){
+// var sum = [finalArray[i]].reduce(add, (i));
+
+// function add(a, b) {
+//     return (finalArray[0] + finalArray[1] + finalArray[2] + finalArray[3] + finalArray[4] + finalArray[5]);
+
+// }
+// }
+// console.log(sum);
+
+//=============================================================
+
+// //const array1 = ['4','3','1','5','1','4'];
+// //const array2 = ['5','5','2','1','3','5'];
+
+//  let finalArray = [];
+//   for(let i = 0; (i < 6); i++){
+
+//     finalArray.push(Math.abs(array1[i] - array2[i]))
+    
+//  }
+// console.log(finalArray[0],finalArray[1],finalArray[2],finalArray[3],finalArray[4],finalArray[5]);
+
+// for(let i = 0; (i < 6); i++){
+// var sum = [finalArray[i]].reduce(add, (i));
+
+// function add(a, b) {
+//     return (finalArray[0] + finalArray[1] + finalArray[2] + finalArray[3] + finalArray[4] + finalArray[5]);
+
+// }
+// }
+// console.log(sum);
+
+>>>>>>> 42fffd49078276483790de3a70001cec94f630f2
--- data/friends.js
+++ data/friends.js
@@ -37,6 +37,61 @@ const friendArray = [
     ];
     module.exports = friendArray;
 
+//Below we create on-click event for survey.html submit button to store input data as 2 objects within the newFriend Array
+    $(".submit").on("click", function(){
 
-
+        // Here we grab the form elements
+        var newFriend = [{
+            name: $('#name').val().trim(),
+            photo: $('#photo').val().trim(),
+            age: $('#age').val().trim()
+        },
+        {
+            group1: $('#group1').val().trim(),
+            group2: $('#group2').val().trim(),
+            group3: $('#group3').val().trim(),
+            group4: $('#group4').val().trim(),
+            group5: $('#group5').val().trim(),
+            group6: $('#group6').val().trim(),
+            group7: $('#group7').val().trim(),
+            group8: $('#group8').val().trim(),
+            group9: $('#group9').val().trim(),
+            group10: $('#group10').val().trim(),
+        }];
+        
+        console.log(newFriend);
+        const newProfile = newFriend[1];
+        console.log(newProfile);
+        module.exports = newProfile;
+    
+    // 		// Essentially we give it a URL, we give it the object we want to send, then we have a "callback".
+    // 		// The callback is the response of the server. In our case, we set up code in api-routes that "returns" true or false
+    // 		// Here we get the location of the root page.
+    // 		// We use this instead of explicitly saying the URL is localhost:3001 because the url will change when we deploy.
+        var currentURL = window.location.origin;
+    
+        $.post(currentURL + `/api/friends`, newProfile,
+        function(data){
+            if (data == true){
+                alert("You are officially added!")
+            }
+    
+            // Clear the form on submit
+            $('#name').val("");
+            $('#photo').val("");
+            $('#age').val("");
+            $('#group1').val("");
+            $('#group2').val("");
+            $('#group3').val("");
+            $('#group4').val("");
+            $('#group5').val("");
+            $('#group6').val("");
+            $('#group7').val("");
+            $('#group8').val("");
+            $('#group9').val("");
+            $('#group10').val("");
+    
+        });
+        return false;
+    });
     
